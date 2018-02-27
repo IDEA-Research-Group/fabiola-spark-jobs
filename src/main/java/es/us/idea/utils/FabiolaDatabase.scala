2f239a7275da7e069f724a34680bb16fa95fd32f
Alvaro <alvarovalenciavp@gmail.com>
2018-02-27 13:19:04 +0100

    deleted the Main and MainGen objects, and created COPJob with the functionality of MainGen. Code refactorized, and added instanceId to the results as an object id

--- src/main/java/es/us/idea/utils/FabiolaDatabase.scala
+++ src/main/java/es/us/idea/utils/FabiolaDatabase.scala
@@ -11,17 +11,34 @@ import scala.concurrent.duration._
 
 import scala.concurrent.Await
 
+/** This class represents a connection with the Fabiola MongoDB database.
+  *
+  * @constructor create a new connection to the Fabiola MongoDB database from the URI of the database and the
+  *              database name.
+  * @param databaseUri The URI to the MongoDB database.
+  * @param databaseName The name of the database used by Fabiola.
+  */
 class FabiolaDatabase(databaseUri: String, databaseName: String) {
   val database = MongoClient(databaseUri).getDatabase(databaseName)
   val instanceCodecRegistry = fromRegistries(fromProviders(classOf[Instance]), DEFAULT_CODEC_REGISTRY)
   val modelDefinitionCodecRegistry = fromRegistries(fromProviders(classOf[ModelDefinition]), DEFAULT_CODEC_REGISTRY)
 
+  /** Gets an instance given its MongoDB ID
+    *
+    * @param instanceId The MongoDB ID of the instance to find.
+    * @return an instance whose _id field is instanceId
+    */
   def getInstance(instanceId: String): Instance = {
     val instances: MongoCollection[Instance] = database.withCodecRegistry(instanceCodecRegistry).getCollection("instances")
     val instanceFuture = instances.find(equal("_id", BsonObjectId(instanceId))).first().toFuture()
     Await.result(instanceFuture, 30 seconds)
   }
 
+  /** Gets a model definition given its MongoDB ID
+    *
+    * @param modelDefinitionId The MongoDB ID of the model defintiion to find.
+    * @return a model defintion whose _id field is instanceId
+    */
   def getModelDefinition(modelDefinitionId: String): ModelDefinition = {
     val modelDefinitions: MongoCollection[ModelDefinition] = database.withCodecRegistry(modelDefinitionCodecRegistry).getCollection("modelDefinitions")
     val modelDefinitionFuture = modelDefinitions.find(equal("_id", BsonObjectId(modelDefinitionId))).first().toFuture()
