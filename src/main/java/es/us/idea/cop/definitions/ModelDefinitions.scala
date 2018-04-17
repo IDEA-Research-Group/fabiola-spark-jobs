41d4540774fb720441f9c2572cf29c3de7e18ee7
Álvaro Valencia <avalenciaparra@gmail.com>
2018-04-17 12:27:30 +0200

    refactor: ModelDefinition now specifies the COP in one field: definition

--- src/main/java/es/us/idea/cop/definitions/ModelDefinitions.scala
+++ src/main/java/es/us/idea/cop/definitions/ModelDefinitions.scala
@@ -11,8 +11,6 @@ object ModelDefinitions {
     |    val scale = 10
     |    val consumoActual = in.get("consumo").get.asInstanceOf[Seq[Map[String, Any]]]
     |    val precioTarifa = in.get("precioTarifa").get.asInstanceOf[Map[String, Double]]
-  """.stripMargin,
-  """
     |    val TP = model.intVarArray("gasto mensual", consumoActual.length, IntVar.MIN_INT_BOUND, IntVar.MAX_INT_BOUND)
     |    val TPTotal = model.intVar("Coste total", IntVar.MIN_INT_BOUND, IntVar.MAX_INT_BOUND)
     |
@@ -24,9 +22,6 @@ object ModelDefinitions {
     |    potenciaContratada(0) = model.intVar("Potencia Contratada 1", 0, IntVar.MAX_INT_BOUND)
     |    potenciaContratada(1) = model.intVar("Potencia Contratada 2", 0, IntVar.MAX_INT_BOUND)
     |    potenciaContratada(2) = model.intVar("Potencia Contratada 3", 0, IntVar.MAX_INT_BOUND)
-    |
-      """.stripMargin,
-  """
     |    for (i <- 0 until consumoActual.length) {
     |      for (j <- 0 until 3) {
     |        val pm = scale * ( j match {
@@ -50,7 +45,7 @@ object ModelDefinitions {
     |          case 2 => precioTarifa.get("p3").get.toInt
     |        }
     |
-    |        val dias = consumoActual(i).get("diasFacturacion").get.asInstanceOf[Long].toInt
+    |        val dias = consumoActual(i).get("diasFacturacion").get.asInstanceOf[Int].toInt
     |
     |        model.ifThen(
     |          model.arithm(model.intScaleView(potenciaContratada(j), 85), ">", pm * 100),
@@ -68,41 +63,36 @@ object ModelDefinitions {
     |    }
     |    for (i <- 0 until TP.length)
     |      model.sum(terminoPotencia(i), "=", TP(i)).post()
+    |    model.sum(TP, "=", TPTotal).post()
+    |    model.setObjective(Model.MINIMIZE, TPTotal)
     |
-      """.stripMargin,
-    """
-      |    model.sum(TP, "=", TPTotal).post()
-      |    model.setObjective(Model.MINIMIZE, TPTotal)
-      |
-      """.stripMargin,
-    """
-      |    val solver = model.getSolver
-      |    val solution = solver.findOptimalSolution(TPTotal, Model.MINIMIZE, new TimeCounter(model, timeout*1000000L))
-      |
-      |    val metrics = Seq(solver.getTimeCount.toDouble,solver.getReadingTimeCount.toDouble, (solver.getTimeCount + solver.getReadingTimeCount).toDouble, model.getNbVars.toDouble, model.getNbCstrs.toDouble)
-      |
-      |    if(solution != null){
-      |      ModelOutput(
-      |        Seq(
-      |          solution.getIntVal(TPTotal) / (100.0 * scale),
-      |          solution.getIntVal(potenciaContratada(0)) / scale.toDouble,
-      |          solution.getIntVal(potenciaContratada(1)) / scale.toDouble,
-      |          solution.getIntVal(potenciaContratada(2)) / scale.toDouble
-      |        ),
-      |        metrics
-      |      )
-      |    } else {
-      |      ModelOutput(
-      |        Seq(
-      |          Default.DefaultDouble.default,
-      |          Default.DefaultDouble.default,
-      |          Default.DefaultDouble.default,
-      |          Default.DefaultDouble.default
-      |        ),
-      |        metrics
-      |      )
-      |    }
-    """.stripMargin
+    |    val solver = model.getSolver
+    |    val solution = solver.findOptimalSolution(TPTotal, Model.MINIMIZE, new TimeCounter(model, timeout*1000000L))
+    |
+    |    val metrics = Seq(solver.getTimeCount.toDouble,solver.getReadingTimeCount.toDouble, (solver.getTimeCount + solver.getReadingTimeCount).toDouble, model.getNbVars.toDouble, model.getNbCstrs.toDouble)
+    |
+    |    if(solution != null){
+    |      ModelOutput(
+    |        Seq(
+    |          solution.getIntVal(TPTotal) / (100.0 * scale),
+    |          solution.getIntVal(potenciaContratada(0)) / scale.toDouble,
+    |          solution.getIntVal(potenciaContratada(1)) / scale.toDouble,
+    |          solution.getIntVal(potenciaContratada(2)) / scale.toDouble
+    |        ),
+    |        metrics
+    |      )
+    |    } else {
+    |      ModelOutput(
+    |        Seq(
+    |          Default.DefaultDouble.default,
+    |          Default.DefaultDouble.default,
+    |          Default.DefaultDouble.default,
+    |          Default.DefaultDouble.default
+    |        ),
+    |        metrics
+    |      )
+    |    }
+  """.stripMargin
   )
 
   val endesaDef = ModelDefinition(
@@ -112,8 +102,6 @@ object ModelDefinitions {
       |    val consumoActual = in.get("consumo_cliente").get.asInstanceOf[Seq[Map[String, Any]]]
       |    val scale = 10
       |    val precioTarifa = Map("p1" -> 11, "p2" -> 7, "p3" -> 4)
-    """.stripMargin,
-    """
       |    val TP = model.intVarArray("gasto mensual", consumoActual.length, IntVar.MIN_INT_BOUND, IntVar.MAX_INT_BOUND)
       |    val TPTotal = model.intVar("Coste total", IntVar.MIN_INT_BOUND, IntVar.MAX_INT_BOUND)
       |
@@ -126,8 +114,6 @@ object ModelDefinitions {
       |    potenciaContratada(1) = model.intVar("Potencia Contratada 2", 0, IntVar.MAX_INT_BOUND)
       |    potenciaContratada(2) = model.intVar("Potencia Contratada 3", 0, IntVar.MAX_INT_BOUND)
       |
-      """.stripMargin,
-    """
       |    for (i <- 0 until consumoActual.length) {
       |      for (j <- 0 until 3) {
       |        val pm = scale * ( j match {
@@ -151,7 +137,7 @@ object ModelDefinitions {
       |          case 2 => precioTarifa.get("p3").get.toInt
       |        }
       |
-      |        val dias = consumoActual(i).get("dias_facturacion").get.asInstanceOf[Long].toInt
+      |        val dias = consumoActual(i).get("dias_facturacion").get.asInstanceOf[Int].toInt
       |
       |        model.ifThen(
       |          model.arithm(model.intScaleView(potenciaContratada(j), 85), ">", pm * 100),
@@ -171,13 +157,9 @@ object ModelDefinitions {
       |    for (i <- 0 until TP.length)
       |      model.sum(terminoPotencia(i), "=", TP(i)).post()
       |
-      """.stripMargin,
-    """
       |    model.sum(TP, "=", TPTotal).post()
       |    model.setObjective(Model.MINIMIZE, TPTotal)
       |
-      """.stripMargin,
-    """
       |    val solver = model.getSolver
       |    val solution = solver.findOptimalSolution(TPTotal, Model.MINIMIZE, new TimeCounter(model, timeout*1000000L))
       |
@@ -214,8 +196,6 @@ object ModelDefinitions {
       |    val consumoActual = in.get("consumos").get.asInstanceOf[Seq[Map[String, Any]]]
       |    val scale = 10
       |    val precioTarifa = in.get("precio_potencia").get.asInstanceOf[Map[String, Double]]
-    """.stripMargin,
-    """
       |    val TP = model.intVarArray("gasto mensual", consumoActual.length, IntVar.MIN_INT_BOUND, IntVar.MAX_INT_BOUND)
       |    val TPTotal = model.intVar("Coste total", IntVar.MIN_INT_BOUND, IntVar.MAX_INT_BOUND)
       |
@@ -228,56 +208,50 @@ object ModelDefinitions {
       |    potenciaContratada(1) = model.intVar("Potencia Contratada 2", 0, IntVar.MAX_INT_BOUND)
       |    potenciaContratada(2) = model.intVar("Potencia Contratada 3", 0, IntVar.MAX_INT_BOUND)
       |
-      """.stripMargin,
-    """
-    for (i <- 0 until consumoActual.length) {
- |      for (j <- 0 until 3) {
- |        val pm = scale * ( j match {
- |          case 0 => math.max(
- |            consumoActual(i).get("potencia_maxima_p1").get.asInstanceOf[Long].toInt,
- |            consumoActual(i).get("potencia_maxima_p1").get.asInstanceOf[Long].toInt
- |          )
- |          case 1 => math.max(
- |            consumoActual(i).get("potencia_maxima_p2").get.asInstanceOf[Long].toInt,
- |            consumoActual(i).get("potencia_maxima_p5").get.asInstanceOf[Long].toInt
- |          )
- |          case 2 => math.max(
- |            consumoActual(i).get("potencia_maxima_p3").get.asInstanceOf[Long].toInt,
- |            consumoActual(i).get("potencia_maxima_p6").get.asInstanceOf[Long].toInt
- |          )
- |        })
- |
- |        val precio = j match {
- |          case 0 => precioTarifa.get("p1").get.toInt
- |          case 1 => precioTarifa.get("p2").get.toInt
- |          case 2 => precioTarifa.get("p3").get.toInt
- |        }
- |
- |        val dias = consumoActual(i).get("dias_facturacion").get.asInstanceOf[Long].toInt
- |
- |        model.ifThen(
- |          model.arithm(model.intScaleView(potenciaContratada(j), 85), ">", pm * 100),
- |          model.scalar(Array(potenciaFactura(i)(j), potenciaContratada(j)), Array(100, -85), "=", 0)
- |        )
- |        model.ifThen(model.arithm(model.intScaleView(potenciaContratada(j), 105), "<", pm * 100),
- |          model.scalar(Array(potenciaFactura(i)(j), potenciaContratada(j)), Array(100, 210), "=", 300*pm)
- |        )
- |
- |        model.ifThen(model.and(model.arithm(model.intScaleView(potenciaContratada(j), 85), "<=", pm * 100), model.arithm(model.intScaleView(potenciaContratada(j), 105), ">=", pm * 100)),
- |          model.scalar(Array(potenciaFactura(i)(j)), Array(100), "=", pm * 100))
- |
- |        model.times(potenciaFactura(i)(j), precio * dias, terminoPotencia(i)(j)).post
- |      }
- |    }
- |    for (i <- 0 until TP.length)
- |      model.sum(terminoPotencia(i), "=", TP(i)).post()
-      """.stripMargin,
-    """
+      |  for (i <- 0 until consumoActual.length) {
+      |      for (j <- 0 until 3) {
+      |        val pm = scale * ( j match {
+      |          case 0 => math.max(
+      |            consumoActual(i).get("potencia_maxima_p1").get.asInstanceOf[Int].toInt,
+      |            consumoActual(i).get("potencia_maxima_p1").get.asInstanceOf[Int].toInt
+      |          )
+      |          case 1 => math.max(
+      |            consumoActual(i).get("potencia_maxima_p2").get.asInstanceOf[Int].toInt,
+      |            consumoActual(i).get("potencia_maxima_p5").get.asInstanceOf[Int].toInt
+      |          )
+      |          case 2 => math.max(
+      |            consumoActual(i).get("potencia_maxima_p3").get.asInstanceOf[Int].toInt,
+      |            consumoActual(i).get("potencia_maxima_p6").get.asInstanceOf[Int].toInt
+      |          )
+      |        })
+      |
+      |        val precio = j match {
+      |          case 0 => precioTarifa.get("p1").get.toInt
+      |          case 1 => precioTarifa.get("p2").get.toInt
+      |          case 2 => precioTarifa.get("p3").get.toInt
+      |        }
+      |
+      |        val dias = consumoActual(i).get("dias_facturacion").get.asInstanceOf[Int].toInt
+      |
+      |        model.ifThen(
+      |          model.arithm(model.intScaleView(potenciaContratada(j), 85), ">", pm * 100),
+      |          model.scalar(Array(potenciaFactura(i)(j), potenciaContratada(j)), Array(100, -85), "=", 0)
+      |        )
+      |        model.ifThen(model.arithm(model.intScaleView(potenciaContratada(j), 105), "<", pm * 100),
+      |          model.scalar(Array(potenciaFactura(i)(j), potenciaContratada(j)), Array(100, 210), "=", 300*pm)
+      |        )
+      |
+      |        model.ifThen(model.and(model.arithm(model.intScaleView(potenciaContratada(j), 85), "<=", pm * 100), model.arithm(model.intScaleView(potenciaContratada(j), 105), ">=", pm * 100)),
+      |          model.scalar(Array(potenciaFactura(i)(j)), Array(100), "=", pm * 100))
+      |
+      |        model.times(potenciaFactura(i)(j), precio * dias, terminoPotencia(i)(j)).post
+      |      }
+      |    }
+      |    for (i <- 0 until TP.length)
+      |      model.sum(terminoPotencia(i), "=", TP(i)).post()
       |    model.sum(TP, "=", TPTotal).post()
       |    model.setObjective(Model.MINIMIZE, TPTotal)
       |
-      """.stripMargin,
-    """
       |    val solver = model.getSolver
       |    val solution = solver.findOptimalSolution(TPTotal, Model.MINIMIZE, new TimeCounter(model, timeout*1000000L))
       |
@@ -309,11 +283,7 @@ object ModelDefinitions {
 
 
   def main(args: Array[String]) = {
-    println(conquenseDef.domainData.replaceAll("\n", "\\\\n").replaceAll("\"", "\\\\\""))
-    println(conquenseDef.variables.replaceAll("\n", "\\\\n").replaceAll("\"", "\\\\\""))
-    println(conquenseDef.constraints.replaceAll("\n", "\\\\n").replaceAll("\"", "\\\\\""))
-    println(conquenseDef.objective.replaceAll("\n", "\\\\n").replaceAll("\"", "\\\\\""))
-    println(conquenseDef.solution.replaceAll("\n", "\\\\n").replaceAll("\"", "\\\\\""))
+    println(conquenseDef.definition.replaceAll("\n", "\\\\n").replaceAll("\"", "\\\\\""))
   }
 
 }
--- src/main/java/es/us/idea/dao/ModelDefinition.scala
+++ src/main/java/es/us/idea/dao/ModelDefinition.scala
@@ -6,16 +6,7 @@ import org.mongodb.scala.bson.BsonObjectId
 /**
   * This case class is intended to contain the model definition
   * written in Scala code, using the Choco Solver library methods.
-  *
-  * @param name        : Name of this model definition
-  * @param domainData  : Data shared by all the Constraint Optimization
-  *                    Problem instances
-  * @param variables   : Declaration of the variables common to all the
-  *                    Constraint Optimization problem instances
-  * @param constraints : Declaration of the constraints
-  * @param objective   : Definition of the objective function
-  * @param solution    : Definition of the sentence that declares the
-  *                    strategy to follow in order to solve the model
   */
-case class ModelDefinition(name: String, domainData: String, variables: String, constraints: String, objective: String, solution: String, _id: Option[BsonObjectId] = None)
+//case class ModelDefinition(name: String, domainData: String, variables: String, constraints: String, objective: String, solution: String, _id: Option[BsonObjectId] = None)
+case class ModelDefinition(name: String, definition: String /*, _id: Option[BsonObjectId] = None*/)
 
