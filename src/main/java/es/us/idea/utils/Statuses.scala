80c249f5b99a7245e28e26f8cdf3cce773a2c619
Álvaro Valencia <avalenciaparra@gmail.com>
2018-04-19 12:39:37 +0200

    refactor: changed Dataset statuses

--- src/main/java/es/us/idea/utils/Statuses.scala
+++ src/main/java/es/us/idea/utils/Statuses.scala
@@ -1,9 +1,14 @@
 package es.us.idea.utils
 
 object Statuses extends Enumeration {
-  val RUNNING     = Value("RUNNING")
-  val WAITING     = Value("WAITING")
-  val ERROR       = Value("ERROR")
-  val NOT_STARTED = Value("NOT_STARTED")
-  val FINISHED    = Value("FINISHED")
+  val RUNNING          = Value("RUNNING")
+  val WAITING          = Value("WAITING")
+  val ERROR            = Value("ERROR")
+  val NOT_STARTED      = Value("NOT_STARTED")
+  val FINISHED         = Value("FINISHED")
+
+  // Specific for Dataset objects
+  val NOT_VALIDATED    = Value("NOT_VALIDATED")
+  val VALIDATED        = Value("VALIDATED")
+  val VALIDATION_ERROR = Value("VALIDATION_ERROR")
 }
